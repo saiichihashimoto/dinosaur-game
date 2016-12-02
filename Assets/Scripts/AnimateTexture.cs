39d3992d1b83f4c596bfc4f18affb29f27cad201
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-12-01 16:46:41 -0800

    U mad bro?

---
+++ Assets/Scripts/AnimateTexture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateTexture : MonoBehaviour {
+	public Renderer renderer = null;
+	public Vector2 direction = Vector2.up;
+	public float movementSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTextureOffset += direction.normalized * Time.deltaTime * movementSpeed / transform.localScale.x * renderer.material.mainTextureScale.x;
+	}
+}
