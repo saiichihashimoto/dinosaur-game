be0ab4b23bc135db5150f0cca818ae6322639894
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-12-09 14:15:20 -0800

    More cloud work

---
+++ Assets/Scripts/SpawnClouds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnClouds : MonoBehaviour {
+	public Level level = null;
+	public GameObject cloud = null;
+	public float minimumSpawnTime = 1;
+	public float maximumSpawnTime = 1;
+	public float minimumCloudHeight = 1;
+	public float maximumCloudHeight = 1;
+
+	// Use this for initialization
+	void Start() {
+		StartCoroutine(SpawnObstacle());
+	}
+
+	private IEnumerator SpawnObstacle() {
+		GameObject obstacle = (GameObject) Instantiate(cloud);
+		obstacle.transform.position = new Vector3(transform.localScale.x/2, minimumCloudHeight + Random.value * (maximumCloudHeight - minimumCloudHeight), 0);
+		obstacle.GetComponent<MoveConstantly>().level = level;
+
+		yield return new WaitForSeconds(minimumSpawnTime + Random.value * (maximumSpawnTime - minimumSpawnTime));
+		StartCoroutine(SpawnObstacle());
+	}
+}
