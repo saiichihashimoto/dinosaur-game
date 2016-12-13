7f1f8275b9f9711475fada5735bce38e28bf04d1
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-12-12 18:08:40 -0800

    Better camera angle and terrain
     #1 #3

---
+++ Assets/Scripts/RestartTerrain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartTerrain : MonoBehaviour {
+	public SpriteRenderer renderer;
+	public int terrainOffset = 0;
+
+	// Use this for initialization
+	void Start () {
+		transform.position += terrainOffset * renderer.bounds.size.x * Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > -renderer.bounds.size.x) {
+			return;
+		}
+		transform.position += 2 * renderer.bounds.size.x * Vector3.right;
+	}
+}
