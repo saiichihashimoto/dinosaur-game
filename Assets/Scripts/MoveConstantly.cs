cf7500d19b2d59a0548af803e91d3381fee87751
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-12-16 03:29:05 -0800

    Moving moon

--- Assets/Scripts/MoveConstantly.cs
+++ Assets/Scripts/MoveConstantly.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class MoveConstantly : MonoBehaviour {
+	public float speedMultiplier = 1;
+	public float restartAt = -5000;
+	private Vector3 startAt;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		startAt = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update() {
+		transform.position += Vector3.left * Time.deltaTime * speedMultiplier;
+		if (transform.position.x > restartAt) {
+			return;
+		}
+		transform.position = startAt;
 	}
 }
