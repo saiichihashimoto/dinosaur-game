99964bed0e9b2f37efde603e7957623e58be596c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-12-20 20:40:47 -0800

    Position, alpha, and twinkling for stars.
    Closes #13

---
+++ Assets/Scripts/RepositionStar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepositionStar : MonoBehaviour {
+	public TimeOfDay timeOfDay = null;
+	public float minimumStart = -500f;
+	public float maximumStart = 500f;
+	private bool isNight = false;
+
+	void Start () {
+	}
+	
+	void Update () {
+		if (!isNight && timeOfDay.isNight()) {
+			transform.position = new Vector3(minimumStart + Random.value * (maximumStart - minimumStart), 0.3333333333f + Random.value * 4.6666666667f, 0f);
+			Animator animator = GetComponent<Animator>();
+			AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+			animator.Play(state.fullPathHash, -1, Random.value);
+		}
+		isNight = timeOfDay.isNight();
+	}
+}
