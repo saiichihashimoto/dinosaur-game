bbec7c1bd71bb3515021ca9a5785e3ce22e3dc41
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-12-25 12:08:55 -0800

    Blink scores every hundred
     #15

--- Assets/Scripts/Scores.cs
+++ Assets/Scripts/Scores.cs
@@ -8,13 +8,23 @@ public class Scores : MonoBehaviour {
 	public AudioClip audioClip = null;
 	public Number score = null;
 	public Number highScore = null;
-	private float lastHundred = 0;
+	public Number flashScore = null;
+	public int flashIterations = 10;
+	public float flashDuration = 1;
+	private int lastHundred = 0;
+	private Vector3 scoreStartPosition = Vector3.zero;
+	private int iterated = 0;
 	private static float highestScore = 0;
+
+	void Start() {
+		flashScore.transform.position = score.transform.position + Vector3.up * 50;
+	}
 
 	void Update() {
 		if (audioSource && audioClip && (int) (level.getDistance() / 100) != lastHundred) {
-			audioSource.PlayOneShot(audioClip, 1);
 			lastHundred = (int) (level.getDistance() / 100);
+			flashScore.Value = lastHundred * 100;
+			StartCoroutine(FlashScore());
 		}
 		score.Value = (int) level.getDistance();
 		highScore.Value = (int) highestScore;
@@ -26,5 +36,27 @@ public class Scores : MonoBehaviour {
 
 	void OnDestroy() {
 		highestScore = Mathf.Max(level.getDistance(), highestScore);
+	}
+
+	IEnumerator FlashScore() {
+		scoreStartPosition = score.transform.position;
+		score.transform.position = scoreStartPosition + Vector3.up * 50;
+		iterated = 0;
+		audioSource.PlayOneShot(audioClip, 1);
+		return FlashScore(false);
+	}
+
+	IEnumerator FlashScore(bool showScore) {
+		flashScore.transform.position = scoreStartPosition + (showScore ? Vector3.zero : Vector3.up * 50);
+		if (showScore) {
+			iterated++;
+		}
+		if (iterated >= flashIterations) {
+			score.transform.position = scoreStartPosition;
+			flashScore.transform.position = scoreStartPosition + Vector3.up * 50;
+		} else {
+			yield return new WaitForSeconds(flashDuration);
+			StartCoroutine(FlashScore(!showScore));
+		}
 	}
 }
